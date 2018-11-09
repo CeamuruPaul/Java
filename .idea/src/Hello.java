bdb6f285bad55174a9d7a62e7eb014c302f424a7
CeamuruPaul <ceamuru.paul@yahoo.ro>
2018-11-09 19:29:25 +0200

    Abc

---
+++ .idea/src/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+    public static void main(String[] args) {
+        int myAge = 20;
+        int anAge = myAge;
+        double weight = 74.2;
+        double height = 376.21;
+        long distance = 1234567;
+        char gender = 'm';
+        char test = 9;
+        boolean istudent = false;
+        boolean hasjob = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+        String mesaj1 = "Hello ";
+        String mesaj2 = "Goodbye";
+        System.out.println(mesaj1);
+
+        int a = 4;
+        int b =2;
+        int c = a - b;
+        System.out.println(c);
+
+        String Mesaj = mesaj1 + mesaj2;
+        System.out.println(Mesaj);
+    }
+}
